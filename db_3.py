3f280f92ec6b0af269c8bcc83cfb1e16c2d18f68
ArgelTorres <argel.torres@cetys.edu.mx>
2018-04-17 19:08:05 -0700

    Update db_3.py

--- db_3.py
+++ db_3.py
@@ -1,25 +1,33 @@
 import kivy
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
+#from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.uix.image import Image
 from kivy.config import Config
 from kivy.uix.button import Button
 
-class LoginScreen(GridLayout):
+
+class LoginScreen(BoxLayout):
     def __init__(self, **kwargs):
-        Config.set('graphics', 'width', '500')
-        Config.set('graphics', 'height', '150')
+        Config.set('graphics', 'width', '300')
+        Config.set('graphics', 'height', '300')
         super(LoginScreen,self).__init__(**kwargs)
-        self.cols = 2
+        #self.cols = 1
+        self.orientation = 'vertical'
+        self.padding = [50,0,50,0]
+        self.image = Image(source='Policía.png', size_hint_y = None, height = 150)
+        self.add_widget(self.image)
         self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
+        self.username = TextInput(multiline=False,size_hint_y = None, height = 30)
         self.add_widget(self.username)
-        self.add_widget(Label(text='password'))
-        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(Label(text='Password'))
+        self.password = TextInput(password=True, multiline=False,size_hint_y = None, height = 30)
         self.add_widget(self.password)
-        self.loginBtn = Button(text="Login")
+        self.loginBtn = Button(text="Login",size_hint_y = None, height = 30)
         self.add_widget(self.loginBtn)
+
 
 class MyApp(App):
 
