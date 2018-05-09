8c03124cc23c0608bae54193455986ad04b98ad3
ddaniel17 <dblancas@cetys.edu.mx>
2018-05-08 19:14:26 -0700

    Add files via upload
    TJPD_DATA

---
+++ TJPD_DATA.sql
@@ -0,0 +1,69 @@
+INSERT INTO TJPD.OFFICER
+VALUES (1, 'David', 'Rookie', 'uno', '1');
+INSERT INTO TJPD.OFFICER
+VALUES (2, 'Luis', 'Policia', 'dos', '2');
+INSERT INTO TJPD.OFFICER
+VALUES (3, 'Juan', 'Comisionado', 'tres', '3');
+INSERT INTO TJPD.OFFICER
+VALUES (4, 'Paco', 'Chief', 'cuatro', '4');
+INSERT INTO TJPD.OFFICER
+VALUES (5, 'Jose', 'Detective', 'cinco', '5');
+INSERT INTO TJPD.OFFICER
+VALUES (6, 'Mario', 'Rookie', 'seis', '6');
+INSERT INTO TJPD.OFFICER
+VALUES (7, 'Gama', 'Policia', 'siete', '7');
+INSERT INTO TJPD.OFFICER
+VALUES (8, 'Alberto', 'Detective', 'ocho', '8');
+INSERT INTO TJPD.OFFICER
+VALUES (9, 'Alfredo', 'Comisionado', 'nueve', '9');
+INSERT INTO TJPD.OFFICER
+VALUES (10, 'Enrique', 'Chief', 'diez', '10');
+INSERT INTO TJPD.OFFICER
+VALUES (11, 'Ignacio', 'Rookie', 'diezuno', '11');
+INSERT INTO TJPD.OFFICER
+VALUES (12, 'Baldo', 'Policia', 'diezdos', '12');
+INSERT INTO TJPD.OFFICER
+VALUES (13, 'Saul', 'Comisionado', 'dieztres', '13');
+INSERT INTO TJPD.OFFICER
+VALUES (14, 'Raul', 'Chief', 'diezcuatro', '14');
+INSERT INTO TJPD.OFFICER
+VALUES (15, 'Alex', 'Detective', 'cinco', '15');
+INSERT INTO TJPD.OFFICER
+VALUES (16, 'Rafa', 'Rookie', 'diezseis', '16');
+INSERT INTO TJPD.OFFICER
+VALUES (17, 'Ramiro', 'Policia', 'diezsiete', '17');
+INSERT INTO TJPD.OFFICER
+VALUES (18, 'Juan', 'Detective', 'diezocho', '18');
+INSERT INTO TJPD.OFFICER
+VALUES (19, 'Alan', 'Comisionado', 'dieznueve', '19');
+INSERT INTO TJPD.OFFICER
+VALUES (20, 'Fer', 'Chief', 'veinte', '20');
+
+INSERT INTO TJPD.CRIME_TYPE
+VALUES (1,'Robo'); 
+
+INSERT INTO TJPD.CRIME_TYPE
+VALUES
+(2,'Asalto');
+
+INSERT INTO TJPD.CRIME_TYPE
+VALUES
+(3,'Secuestro');
+ 
+INSERT INTO TJPD.CRIME_TYPE
+VALUES(4,'Asesinato'); 
+
+INSERT INTO TJPD.CRIME_TYPE
+VALUES
+(5,'Extorsion'); 
+
+INSERT INTO TJPD.CRIME_TYPE
+VALUES
+(6,'Violacion'); 
+INSERT INTO TJPD.CRIME_TYPE
+VALUES
+(7,'Abuso');
+
+INSERT INTO TJPD.CRIME_TYPE
+VALUES
+(8,'Fraude');
