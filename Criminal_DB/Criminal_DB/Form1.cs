81f052d92841de9d7ba547b633c9709c31b6a6a7
ArgelTorres <argel.torres@cetys.edu.mx>
2018-05-21 20:51:37 -0700

    a ver si funciona

--- Criminal_DB/Criminal_DB/Form1.cs
+++ Criminal_DB/Criminal_DB/Form1.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.DataAccess;
+using Oracle.DataAccess.Types;
+using Oracle.DataAccess.Client;
 
 namespace Criminal_DB
 {
@@ -15,6 +19,47 @@ namespace Criminal_DB
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+            {
+                textBox2.PasswordChar = '*';
+                Console.Write("Entra");
+            }
+            else
+            {
+                textBox2.PasswordChar =  '\0';
+            }
+ 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //OracleConnection cn = new OracleConnection(DatabaseHelper.GetConnection)
+            //string oradb = "Data Source=TJPD;User Id=TJPD;Password=Criminal2511;";
+            //OracleConnection conn = new OracleConnection(oradb);  // C#
+            //System.Diagnostics.Debug.WriteLine("Checar");
+            //System.Diagnostics.Debug.WriteLine(conn);
+            //conn.Open();
+            //OracleCommand cmd = conn.CreateCommand();
+            //cmd.CommandText = "begin open :1 for select* from officer where rank = 1; end; ";
+            //OracleParameter p_rc = cmd.Parameters.Add("p_rc",OracleDbType.RefCursor,DBNull.Value,ParameterDirection.Output);
+            //OracleCommand cmd = new OracleCommand();
+            MainForm main_form = new MainForm();
+            main_form.Show();
+            this.Hide();
         }
     }
 }
--- Criminal_DB/Criminal_DB/MainForm.cs
+++ Criminal_DB/Criminal_DB/MainForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Criminal_DB
 {
     public partial class MainForm : Form
@@ -15,6 +16,20 @@ namespace Criminal_DB
         public MainForm()
         {
             InitializeComponent();
+            //Form2 objForm = new Form2();
+            //MainForm.Controls.Add(objForm);
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+            Form2 objForm = new Form2();
+            objForm.TopLevel = false;
+            objForm.FormBorderStyle = FormBorderStyle.None;
+            splitContainer1.Panel2.Controls.Add(objForm);
+            objForm.Visible = true;
+
+            //MainForm.Controls.Add(objForm);
+            
         }
     }
 }
